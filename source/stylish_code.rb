c3fcbb22c8848425183e1642dcd63be81b2320ae
Apprentice <apprentice@dbc28.local>
2014-01-06 21:15:06 -0800

    refactored code formatting

--- source/stylish_code.rb
+++ source/stylish_code.rb
@@ -1,43 +1,43 @@
-class Guessing_game
-  VALID_Numbers = (1..100).to_a # Store valid answers in an array
+class GuessingGame
+  VALID_NUMBERS = (1..100).to_a
 
-  def initialize answer; @answer = answer
-@solved = false
-# Validate input
-  raise "Answer must be between 1 and 100" unless VALID_Numbers.include? @answer
+  def initialize answer;
+    @answer = answer
+    @solved = false
+    raise "Answer must be between 1 and 100" unless VALID_NUMBERS.include? @answer
   end
-  
-def guess ( number )
-if number == @answer # Check if the two are equal
-@solved = true
-      :correct
-  elsif (number > @answer) # Check if the guess is higher
-@solved = false
-    return :high
-elsif(number<@answer) # Check if the guess is lower
+
+  def guess (number)
+    if number == @answer
+      @solved = true
+      return :correct
+    elsif number > @answer
       @solved = false
-    :low
-end
+      return :high
+    else
+      @solved = false
+      return :low
+    end
   end
-  
+
   def solved?
-  @solved
-end
+    @solved
+  end
 end
 
-game = Guessing_game.new(10)
+game = GuessingGame.new(10)
 
 # This following should print out a whole bunch lines of "true"
-puts game.guess(5)  == :low
+puts game.guess(5) == :low
 puts game.guess(15) == :high
-puts game.solved?   == false
+puts game.solved?  == false
 puts game.guess(10) == :correct
-puts game.solved?   == true
-puts game.guess(2)  == :low
-puts game.solved?   == false
+puts game.solved? == true
+puts game.guess(2) == :low
+puts game.solved? == false
 
 begin
-  Guessing_game.new(200)
+  GuessingGame.new(200)
 rescue RuntimeError => e
   puts e.to_s == "Answer must be between 1 and 100"
 end
